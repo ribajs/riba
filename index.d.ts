2fd7b53536a80af963e335d132d4c38971d4f5c9
JumpLink <pascal@jumplink.eu>
2018-06-20 20:32:31 +0200

    [TypeScript] More methods on IBinding interface

--- index.d.ts
+++ index.d.ts
@@ -41,12 +41,70 @@ declare module 'tinybind' {
 
   export interface IBinding {
     view: View;
-    unbind: () => void;
-    observe: (obj: Object, keypath: string, callback: (newValue: any) => void) => Observer;
+    el: HTMLElement;
+    /**
+     * Name of the binder without the prefix
+     */
+    type: string;
+    binder: Binder<any>;
+    formatters: string[];
+    formatterObservers: any[];
     keypath: string;
     arg: string[];
+    model?: any;
+    /**
+     * just to have a value where we could store custom data
+     */
+    data?: any;
+    /**
+     * Subscribes to the model for changes at the specified keypath. Bi-directional
+     * routines will also listen for changes on the element to propagate them back
+     * to the model.
+     */
+    bind: () => void;
+    /**
+     * Returns an event handler for the binding around the supplied function.
+     */
     eventHandler: (handler: (event: Event) => void) => () => any;
-    binderData: any;
+    /**
+     * Applies all the current formatters to the supplied value and returns the
+     * formatted value.
+     */
+    formattedValue: (value: any) => void;
+    /**
+     * Returns elements value
+     */
+    getValue: (models: any) => any;
+
+    parseFormatterArguments: (args: any, formatterIndex: number) => any;
+
+    parseTarget: () => void;
+    /**
+     * Publishes the value currently set on the input element back to the model.
+     */
+    publish: () => any | void;
+    /**
+     * Sets the value for the binding. This Basically just runs the binding routine
+     * with the supplied value formatted.
+     */
+    set: (value: any) => void;
+    /**
+     * Syncs up the view binding with the model.
+     */
+    sync: () => void;
+    /**
+     * Unsubscribes from the model and the element.
+     */
+    unbind: () => void;
+    /**
+     * Updates the binding's model from what is currently set on the view. Unbinds
+     * the old model first and then re-binds with the new model.
+     */
+    update: (models: any) => void;
+    /**
+     * Observes the object keypath
+     */
+    observe: (obj: Object, keypath: string, callback: (newValue: any) => void) => Observer;
   }
 
   export interface IOneWayBinder<ValueType> {
@@ -61,6 +119,8 @@ declare module 'tinybind' {
     getValue?: (this: IBinding, element: HTMLElement) => void;
     block?: boolean;
     function?: boolean;
+    publishes?: boolean;
+    priority?: number;
   }
 
   export type Binder<ValueType> = IOneWayBinder<ValueType> | ITwoWayBinder<ValueType>
