7ef68df9414d8b3dd887110d818905dad892e711
JumpLink <pascal@jumplink.eu>
2018-07-04 22:19:06 +0200

    Rerun the tests also with the minified javascript output file

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,43 @@
 // https://github.com/Microsoft/TypeScript-Babel-Starter
 var path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-    entry: {
-      tinybind: './src/browser.ts'
-    },
-    mode: 'production', // production | development
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: '[name].min.js',
-      library: 'tinybind',
-      libraryTarget: 'umd',
-      libraryExport: 'default'
-    },
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.json']
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(tsx?)|(js)$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-        },
-      ],
-    }
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        sourceMap: false,
+        uglifyOptions: {
+          compress: true,
+          mangle: true,
+          output: {
+            beautify: false,
+          }
+        }
+      })
+    ]
+  },
+  entry: {
+    tinybind: './src/browser.ts'
+  },
+  mode: 'production', // production | development
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].min.js',
+    library: 'tinybind',
+    libraryTarget: 'umd',
+    libraryExport: 'default'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(tsx?)|(js)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  }
 };
