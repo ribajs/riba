b1dff2e1388c775b424e8af02a4977673b03a315
JumpLink <pascal@jumplink.eu>
2018-09-18 19:06:29 +0200

    [Router] New router for pagination and active parent routes

--- src/binders/router/router.binders.ts
+++ src/binders/router/router.binders.ts
@@ -1,9 +1,11 @@
 import { Pjax, Prefetch, GlobalEvent } from './barba/barba';
 import { IBinders } from '../../binder.service';
 import { viewBinderWrapper } from './view.binder';
+import { routeAppendBinderOnStarWrapper } from './route-append-on-star.binder';
 import { viewStaticBinderWrapper } from './view-static.binder';
 import { routeBinderWrapper } from './route.binder';
 import { routeClassStarBinderWrapper } from './route-class-star.binder';
+import { parentRouteClassStarBinderWrapper } from './parent-route-class-star.binder';
 import { routeBackOnStarBinderWrapper } from './route-back-on-star.binder';
 
 const dispatcher = new GlobalEvent();
@@ -12,15 +14,19 @@ const prefetch = new Prefetch();
 const routerBinders: IBinders<any> = {};
 
 const viewBinder = viewBinderWrapper(dispatcher, prefetch);
+const routeAppendOnStarBinder = routeAppendBinderOnStarWrapper();
 const viewStaticBinder = viewStaticBinderWrapper();
 const routeBinder = routeBinderWrapper(dispatcher, prefetch);
 const routeClassStarBinder = routeClassStarBinderWrapper(dispatcher);
+const parentRouteClassStarBinder = parentRouteClassStarBinderWrapper(dispatcher);
 const routeBackOnStarBinder = routeBackOnStarBinderWrapper();
 
 routerBinders[viewBinder.name] = viewBinder.binder;
+routerBinders[routeAppendOnStarBinder.name] = routeAppendOnStarBinder.binder;
 routerBinders[viewStaticBinder.name] = viewStaticBinder.binder;
 routerBinders[routeBinder.name] = routeBinder.binder;
 routerBinders[routeClassStarBinder.name] = routeClassStarBinder.binder;
+routerBinders[parentRouteClassStarBinder.name] = parentRouteClassStarBinder.binder;
 routerBinders[routeBackOnStarBinder.name] = routeBackOnStarBinder.binder;
 
 export { Pjax, Prefetch, GlobalEvent, routerBinders };
