a138839f0c1ec5006304fabb9a95c00e302651df
JumpLink <pascal@jumplink.eu>
2019-08-29 16:52:16 +0200

    [bs4] Fix module type

--- packages/bs4/src/index.ts
+++ packages/bs4/src/index.ts
@@ -1,11 +1,13 @@
+
+import { IRibaModule } from '@ribajs/core';
 export * from './services';
 
 // import * from './interfaces/interfaces';
-import * as binders  from './binders';
+import * as binders from './binders';
 import * as components from './components';
 // import * as formatters from './formatters/bs4.formatters';
 import * as services from './services';
-export const bs4Module = {
+export const bs4Module: IRibaModule = {
   binders,
   services,
   // formatters,
