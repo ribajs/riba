d85e696472c21de25faa6cc1c24e4cd570d8fe94
Pascal Garber <pascal@artandcode.studio>
2019-11-21 06:49:20 -0500

    [Core] Add support for random number (not just random array property)

--- packages/core/src/formatters/array/random.formatter.ts
+++ packages/core/src/formatters/array/random.formatter.ts
@@ -3,13 +3,24 @@ import { Utils } from '../../services/utils';
 /**
  * Gets back random value of array
  * @example <div rv-add-class='"["col-2", "col-3", "col-4", "col-5", "col-6"]" | parse | random'>
+ *
+ * Or gets back a randon number
+ * Random number between 0 and 6:
+ * @example <div rv-add-class='6 | random'>
+ * Random number between 1 and 6:
+ * @example <div rv-add-class='6 | random 1'>
  */
 export const randomFormatter = {
   name: 'random',
-  read(array: any[]) {
-    if (Utils.isArray(array)) {
-      const value = array[Math.floor(Math.random() * array.length)];
-      return value;
+  read(arrayOrMaxNumber: any, min: number = 0) {
+    // If is array
+    if (Utils.isArray(arrayOrMaxNumber)) {
+      return arrayOrMaxNumber[Math.floor(Math.random() * arrayOrMaxNumber.length)];
+    }
+    // If is number
+    if (Utils.isNumber(arrayOrMaxNumber)) {
+      const max = arrayOrMaxNumber as number;
+      return Math.floor(Math.random() * (max - min + 1) + min);
     }
     return null;
   },
