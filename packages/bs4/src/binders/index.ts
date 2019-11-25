5912a4753eb7a241cca953562cd33a852c3d1f9c
JumpLink <pascal@jumplink.eu>
2019-11-25 11:43:15 +0100

    [Bs4] Renamed bs4 binders

--- packages/bs4/src/binders/index.ts
+++ packages/bs4/src/binders/index.ts
@@ -1,7 +1,7 @@
-export { collapseOnUrlBinder } from './collapse-on-url.binder';
-export { collapseBinder } from './collapse.binder';
-export { dropdownBinder } from './dropdown.binder';
-export { expanOnUrlBinder } from './expan-on-url.binder';
-export { scrollspyClassBinder } from './scrollspy-class.binder';
-export { tooltipBinder } from './tooltip.binder';
+export { collapseOnUrlBinder } from './bs4-collapse-on-url.binder';
+export { collapseBinder } from './bs4-collapse.binder';
+export { dropdownBinder } from './bs4-dropdown.binder';
+export { expanOnUrlBinder } from './bs4-expan-on-url.binder';
+export { scrollspyClassBinder } from './bs4-scrollspy-class.binder';
+export { tooltipBinder } from './bs4-tooltip.binder';
 export { scrollToOnEventBinder } from './scroll-to-on-event.binder';
