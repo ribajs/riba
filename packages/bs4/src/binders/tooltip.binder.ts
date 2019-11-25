6d1de9fa4b01abdc4ab6f7eaf58d58e168745991
JumpLink <pascal@jumplink.eu>
2019-11-25 11:32:30 +0100

    [Bs4] Removed JQuery from tooltip binder

--- packages/bs4/src/binders/tooltip.binder.ts
+++ packages/bs4/src/binders/tooltip.binder.ts
@@ -6,11 +6,18 @@ import Popper from 'popper.js'; // /dist/umd/popper
  * @see https://github.com/twbs/bootstrap/blob/v4-dev/js/src/tooltip.js
  */
 import { Binder } from '@ribajs/core';
-import { JQuery as $ } from '@ribajs/jquery';
 
-const template = '<div class="tooltip" role="tooltip">' +
-'<div class="arrow"></div>' +
-'<div class="tooltip-inner"></div></div>';
+const template = document.createElement('div');
+template.classList.add('tooltip');
+template.setAttribute('role', 'tooltip');
+
+const arrow = document.createElement('div');
+arrow.classList.add('arrow');
+template.appendChild(arrow);
+
+const inner = document.createElement('div');
+inner.classList.add('tooltip-inner');
+template.appendChild(inner);
 
 /**
  *
@@ -19,12 +26,13 @@ export const tooltipBinder: Binder<string> = {
   name: 'bs4-tooltip',
   block: false,
   bind(el: HTMLUnknownElement) {
-    this.customData.$tip = $(template);
+    // this.customData.$tip = $(template);
+    this.customData.tip = template.cloneNode(true);
     this.customData.show = () => {
-      const attachment: 'auto' | 'top' | 'right' | 'bottom' | 'left' = 'top';
+      const placement = (this.el.dataset.placement || 'top') as 'auto' | 'top' | 'right' | 'bottom' | 'left';
       const offset = 0;
-      this.customData.popper = new Popper(el, this.customData.$tip[0], {
-        placement: attachment,
+      this.customData.popper = new Popper(el, this.customData.tip, {
+        placement,
         modifiers: {
           offset: {
             offset,
@@ -40,12 +48,12 @@ export const tooltipBinder: Binder<string> = {
           },
         },
       });
-      this.customData.$tip.appendTo(document.body);
-      this.customData.$tip.addClass('show');
-      this.customData.$tip.addClass('bs-tooltip-' + attachment);
+      document.body.appendChild(this.customData.tip);
+      this.customData.tip.classList.add('show');
+      this.customData.tip.classList.add('bs-tooltip-' + placement);
     };
     this.customData.hide = () => {
-      this.customData.$tip.removeClass('show');
+      this.customData.tip.classList.remove('show');
       if (this.customData.popper) {
         this.customData.popper.destroy();
       }
@@ -55,7 +63,8 @@ export const tooltipBinder: Binder<string> = {
   },
 
   routine(el: HTMLElement, text: string) {
-    this.customData.$tip.find('.tooltip-inner').html(text);
+    const innerEl = this.customData.tip.querySelector('.tooltip-inner') as HTMLDivElement;
+    innerEl.innerHTML = text;
   },
 
   unbind() {
