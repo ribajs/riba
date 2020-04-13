a34eed9548b4242f3d2b9a843368ff629690241a
JumpLink <pascal@jumplink.eu>
2020-04-13 14:48:50 +0200

    [Bs4:toggleItemBinder] Fix trigger state back to Bs4ToggleButtonComponent

--- packages/bs4/src/binders/bs4-toggle-item.binder.ts
+++ packages/bs4/src/binders/bs4-toggle-item.binder.ts
@@ -3,6 +3,11 @@ import { Binder, EventDispatcher } from '@ribajs/core';
 export interface Bs4Toggle extends Binder<boolean> {
   toggleButtonEvents: EventDispatcher | null;
   state: 'hidden' | 'visable';
+  triggerState: () => void;
+  onToggle: () => void;
+  toggle: (el: HTMLElement) => void;
+  show: (el: HTMLElement) => void;
+  hide: (el: HTMLElement) => void;
 }
 
 import {
@@ -22,11 +27,13 @@ export const toggleItemBinder: Binder<string> = {
   toggleButtonEvents: null,
   state: 'visable',
   triggerState() {
-    (this.binder as Bs4Toggle).toggleButtonEvents?.trigger(TOGGLE_BUTTON.eventNames.state, (this.binder as Bs4Toggle).state);
+    const self = (this.binder || this) as Bs4Toggle;
+    self.toggleButtonEvents?.trigger(TOGGLE_BUTTON.eventNames.state, self.state);
   },
   onToggle() {
+    const self = (this.binder || this) as Bs4Toggle;
     // console.debug('onToggle', (this.binder as Bs4Toggle));
-    (this.binder as Bs4Toggle).toggle(this.el);
+    self.toggle(this.el);
   },
   toggle(el: HTMLElement) {
     if (this.state === 'hidden') {
@@ -38,35 +45,40 @@ export const toggleItemBinder: Binder<string> = {
   show(el: HTMLElement) {
     el.removeAttribute('hidden');
     this.state = 'visable'
-    el.dispatchEvent(new Event(TOGGLE_ITEM.elEventNames.show))
+    el.dispatchEvent(new Event(TOGGLE_ITEM.elEventNames.show));
+    this.triggerState();
   },
   hide(el: HTMLElement) {
     el.setAttribute('hidden', 'hidden');
     this.state = 'hidden';
-    el.dispatchEvent(new Event(TOGGLE_ITEM.elEventNames.hide))
+    el.dispatchEvent(new Event(TOGGLE_ITEM.elEventNames.hide));
+    this.triggerState();
   },
   bind(el) {
-    (this.binder as Bs4Toggle).state = el.hasAttribute('hidden') ? 'hidden' : 'visable'
+    const self = (this.binder || this) as Bs4Toggle;
+    self.state = el.hasAttribute('hidden') ? 'hidden' : 'visable'
   },
 
   unbind() {
-    (this.binder as Bs4Toggle).toggleButtonEvents?.off(TOGGLE_BUTTON.eventNames.toggle, (this.binder as Bs4Toggle).onToggle.bind(this));
-    (this.binder as Bs4Toggle).toggleButtonEvents?.off(TOGGLE_BUTTON.eventNames.init, (this.binder as Bs4Toggle).triggerState.bind(this));
+    const self = (this.binder || this) as Bs4Toggle;
+    self.toggleButtonEvents?.off(TOGGLE_BUTTON.eventNames.toggle, self.onToggle.bind(this));
+    self.toggleButtonEvents?.off(TOGGLE_BUTTON.eventNames.init, self.triggerState.bind(this));
   },
 
   routine(el: HTMLElement, newId: string) {
     const oldId = this.getValue(el);
-    let toggleButton = (this.binder as Bs4Toggle).toggleButtonEvents;
+    const self = (this.binder || this) as Bs4Toggle;
+    let toggleButton = self.toggleButtonEvents;
     if (oldId && toggleButton) {
-      toggleButton.off(TOGGLE_BUTTON.eventNames.toggle, (this.binder as Bs4Toggle).onToggle.bind(this));
-      toggleButton.off(TOGGLE_BUTTON.eventNames.init, (this.binder as Bs4Toggle).triggerState.bind(this));
+      toggleButton.off(TOGGLE_BUTTON.eventNames.toggle, self.onToggle.bind(this));
+      toggleButton.off(TOGGLE_BUTTON.eventNames.init, self.triggerState.bind(this));
     }
 
-    if(!(this.binder as Bs4Toggle).toggleButtonEvents) {
-      (this.binder as Bs4Toggle).toggleButtonEvents = new EventDispatcher(TOGGLE_BUTTON.nsPrefix + newId);
-      toggleButton = (this.binder as Bs4Toggle).toggleButtonEvents as EventDispatcher;
-      toggleButton.on(TOGGLE_BUTTON.eventNames.toggle, (this.binder as Bs4Toggle).onToggle.bind(this));
-      toggleButton.on(TOGGLE_BUTTON.eventNames.init, (this.binder as Bs4Toggle).triggerState.bind(this));
+    if(!self.toggleButtonEvents) {
+      self.toggleButtonEvents = new EventDispatcher(TOGGLE_BUTTON.nsPrefix + newId);
+      toggleButton = self.toggleButtonEvents as EventDispatcher;
+      toggleButton.on(TOGGLE_BUTTON.eventNames.toggle, self.onToggle.bind(this));
+      toggleButton.on(TOGGLE_BUTTON.eventNames.init, self.triggerState.bind(this));
     }
     
   },
