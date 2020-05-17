ff78cb2319806e62501646f46ae5a652536affcc
Daniel <dotonesimple@gmail.com>
2020-05-17 15:20:55 +0200

    [Leaflet] added custom icon to leaflet component

--- packages/leaflet-map/src/components/leaflet/leaflet-map.component.ts
+++ packages/leaflet-map/src/components/leaflet/leaflet-map.component.ts
@@ -17,6 +17,7 @@ interface Marker {
   lat: number;
   lng: number;
   title: string;
+  icon?: string;
   openByDefault: boolean;
 }
 
@@ -26,6 +27,17 @@ export class LeafletMapComponent extends Component {
   protected autobind = true;
 
   protected markers: Marker[] = [];
+  protected icons: any[] = [];
+
+  protected defaultIcon: any = L.icon({
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+    iconSize: [25, 41],
+    iconAnchor: [14, 40],
+    popupAnchor: [-1, -41],
+    shadowSize: [25, 41],
+    shadowAnchor: [10, 40]
+  });
 
   static get observedAttributes() {
     return [
@@ -68,23 +80,18 @@ export class LeafletMapComponent extends Component {
     this.el.querySelector('.leaflet-map').id = mapId;
     var map = new L.Map(mapId).setView([this.scope.initialLat, this.scope.initialLng], this.scope.initialZoom);;
     
-    var defaultMarker = L.icon({
-      iconUrl: markerIcon,
-      shadowUrl: markerShadow,
-      iconSize: [25, 41],
-      iconAnchor: [14, 40],
-      popupAnchor: [-1, -41],
-      shadowSize: [25, 41],
-      shadowAnchor: [10, 40]
 
-  });
 
     L.tileLayer(this.scope.tileUrl, {
       attribution: this.scope.attribution
     }).addTo(map);
 
     for(let marker of this.markers) {
-      let leatfletMarker = L.marker([marker.lat, marker.lng], {icon: defaultMarker})
+      var icon = this.defaultIcon;  
+      if(marker.icon !== undefined) {
+        icon = this.icons[marker.icon]
+      }
+      let leatfletMarker = L.marker([marker.lat, marker.lng], {icon: icon})
       .addTo(map)
       .bindPopup(marker.title);
       if(marker.openByDefault) {
@@ -102,15 +109,36 @@ export class LeafletMapComponent extends Component {
 
   protected template() {
     for(let el of this.el.children) {
+      if(el.tagName === "ICON") {
+        var iconName = el.getAttribute("name"); 
+        var iconUrl = el.getAttribute("iconUrl");
+        var shadowUrl = el.getAttribute("shadowUrl");
+        var iconSize = el.getAttribute("iconSize"); 
+        var iconAnchor = el.getAttribute("iconAnchor");
+        var popupAnchor = el.getAttribute("popupAnchor");
+        var shadowSize = el.getAttribute("shadowSize");
+        var shadowAnchor = el.getAttribute("shadowAnchor");
+        this.icons[iconName] = L.icon({
+          iconUrl: iconUrl,
+          shadowUrl: shadowUrl,
+          iconSize: iconSize.replace(" ", "").split(","),
+          iconAnchor: iconAnchor.replace(" ", "").split(","),
+          popupAnchor: popupAnchor.replace(" ", "").split(","),
+          shadowSize: shadowSize.replace(" ", "").split(","),
+          shadowAnchor: shadowAnchor.replace(" ", "").split(",")
+        });    
+      }
       if(el.tagName === "MARKER") {
         var lat = el.getAttribute("lat"); 
         var lng = el.getAttribute("lng");
+        var icon = el.getAttribute("icon");
         var title = el.textContent;
-        if(lat !== undefined && lng !== undefined && title !==   undefined) {
+        if(lat !== undefined && lng !== undefined && title !== undefined) {
           this.markers.push({
             lat: lat,
             lng: +lng,
             title: title,
+            icon: icon,
             openByDefault: el.hasAttribute("openByDefault") ? el.getAttribute("openByDefault") === "true" : true
           });
         } else {
