c16a20fcd8512f77fc3158095e7e5dfac457830f
JumpLink <pascal@jumplink.eu>
2020-06-18 20:35:46 +0200

    [Bs4] Ported toaster service from Bootstrap to Riba

--- packages/bs4/src/services/index.ts
+++ packages/bs4/src/services/index.ts
@@ -2,4 +2,5 @@ export * from "./dom";
 export * from "./carousel.service";
 export { CollapseService } from "./collapse.service";
 export { DropdownService } from "./dropdown.service";
+export { ToastService } from "./toast.service";
 export * from "./utils.service";
---
+++ packages/bs4/src/services/toast.service.ts
@@ -0,0 +1,220 @@
+// Fork of https://raw.githubusercontent.com/twbs/bootstrap/main/js/src/toast.js
+
+/**
+ * --------------------------------------------------------------------------
+ * Bootstrap (v5.0.0-alpha1): toast.js
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
+ * --------------------------------------------------------------------------
+ */
+
+import { Utils, TRANSITION_END } from "./utils.service";
+import Data from "./dom/data";
+import EventHandler from "./dom/event-handler";
+
+/**
+ * ------------------------------------------------------------------------
+ * Constants
+ * ------------------------------------------------------------------------
+ */
+
+const NAME = "toast";
+const VERSION = "5.0.0-alpha1-riba";
+const DATA_KEY = "bs.toast";
+const EVENT_KEY = `.${DATA_KEY}`;
+
+export const EVENT_CLICK_DISMISS = `click.dismiss${EVENT_KEY}`;
+export const EVENT_HIDE = `hide${EVENT_KEY}`;
+export const EVENT_HIDDEN = `hidden${EVENT_KEY}`;
+export const EVENT_SHOW = `show${EVENT_KEY}`;
+export const EVENT_SHOWN = `shown${EVENT_KEY}`;
+
+export const CLASS_NAME_FADE = "fade";
+export const CLASS_NAME_HIDE = "hide";
+export const CLASS_NAME_SHOW = "show";
+export const CLASS_NAME_SHOWING = "showing";
+
+export interface Config {
+  animation?: boolean;
+  autohide?: boolean;
+  delay?: number;
+}
+
+export const DefaultType = {
+  animation: "boolean",
+  autohide: "boolean",
+  delay: "number",
+};
+
+export const Default = {
+  animation: true,
+  autohide: true,
+  delay: 500,
+};
+
+// const SELECTOR_DATA_DISMISS = '[data-dismiss="toast"]';
+
+/**
+ * ------------------------------------------------------------------------
+ * Class Definition
+ * ------------------------------------------------------------------------
+ */
+
+export class ToastService {
+  protected _element: HTMLElement;
+  protected _config: Config = {};
+  protected _timeout: number | null = null;
+
+  constructor(element: HTMLElement, config: Config) {
+    this._element = element;
+    this._config = this._getConfig(config);
+    this._timeout = null;
+    this._setListeners();
+    Data.setData(element, DATA_KEY, this);
+  }
+
+  // Getters
+
+  static get VERSION() {
+    return VERSION;
+  }
+
+  static get DefaultType() {
+    return DefaultType;
+  }
+
+  static get Default() {
+    return Default;
+  }
+
+  // Public
+
+  show() {
+    const showEvent = EventHandler.trigger(this._element, EVENT_SHOW);
+
+    if (showEvent.defaultPrevented) {
+      return;
+    }
+
+    if (this._config.animation) {
+      this._element.classList.add(CLASS_NAME_FADE);
+    }
+
+    const complete = () => {
+      if (!this._element) {
+        return;
+      }
+      this._element.classList.remove(CLASS_NAME_SHOWING);
+      this._element.classList.add(CLASS_NAME_SHOW);
+
+      EventHandler.trigger(this._element, EVENT_SHOWN);
+
+      if (this._config.autohide) {
+        this._timeout = setTimeout(() => {
+          this.hide();
+        }, this._config.delay);
+      }
+    };
+
+    this._element.classList.remove(CLASS_NAME_HIDE);
+    Utils.reflow(this._element);
+    this._element.classList.add(CLASS_NAME_SHOWING);
+    if (this._config.animation) {
+      const transitionDuration = Utils.getTransitionDurationFromElement(
+        this._element
+      );
+
+      EventHandler.one(this._element, TRANSITION_END, complete);
+      Utils.emulateTransitionEnd(this._element, transitionDuration);
+    } else {
+      complete();
+    }
+  }
+
+  hide() {
+    if (!this._element.classList.contains(CLASS_NAME_SHOW)) {
+      return;
+    }
+
+    const hideEvent = EventHandler.trigger(this._element, EVENT_HIDE);
+
+    if (hideEvent.defaultPrevented) {
+      return;
+    }
+
+    const complete = () => {
+      if (!this._element) {
+        return;
+      }
+      this._element.classList.add(CLASS_NAME_HIDE);
+      EventHandler.trigger(this._element, EVENT_HIDDEN);
+    };
+
+    this._element.classList.remove(CLASS_NAME_SHOW);
+    if (this._config.animation) {
+      const transitionDuration = Utils.getTransitionDurationFromElement(
+        this._element
+      );
+
+      EventHandler.one(this._element, TRANSITION_END, complete);
+      Utils.emulateTransitionEnd(this._element, transitionDuration);
+    } else {
+      complete();
+    }
+  }
+
+  dispose() {
+    if (this._timeout) {
+      clearTimeout(this._timeout);
+    }
+    this._timeout = null;
+
+    if (this._element.classList.contains(CLASS_NAME_SHOW)) {
+      this._element.classList.remove(CLASS_NAME_SHOW);
+    }
+
+    // EventHandler.off(this._element, EVENT_CLICK_DISMISS);
+    Data.removeData(this._element, DATA_KEY);
+
+    // this._element = null;
+    // this._config = null;
+  }
+
+  // Private
+
+  _getConfig(config: any) {
+    config = {
+      ...Default,
+      ...(this._element?.dataset || {}),
+      ...(typeof config === "object" && config ? config : {}),
+    };
+
+    Utils.typeCheckConfig(NAME, config, ToastService.DefaultType);
+
+    return config;
+  }
+
+  _setListeners() {
+    if (!this._element) {
+      return;
+    }
+    EventHandler.on(
+      this._element,
+      EVENT_CLICK_DISMISS,
+      // SELECTOR_DATA_DISMISS,
+      () => {
+        // const SELECTOR_DATA_DISMISS = '[data-dismiss="toast"]';
+        if (this._element?.dataset.dismiss === "toast") {
+          this.hide();
+        }
+      }
+    );
+  }
+
+  // Static
+
+  static getInstance(element: HTMLElement) {
+    return Data.getData(element, DATA_KEY);
+  }
+}
+
+export default ToastService;
