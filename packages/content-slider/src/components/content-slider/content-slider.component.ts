30bc7376cfb5d3a5eedf3dfb7c9da83854d8d152
art-and-code-studio <pascal@artandcode.studio>
2020-08-27 21:13:58 +0200

    [ContentSlider] Use bootstrap grid classes for item widths

--- packages/content-slider/src/components/content-slider/content-slider.component.ts
+++ packages/content-slider/src/components/content-slider/content-slider.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@ribajs/core";
 
 import template from "./content-slider.component.html";
+import buttonsTemplate from "./content-slider-buttons.component.html";
 
 interface Scope {
   hello?: string;
@@ -9,7 +10,12 @@ interface Scope {
   previous: ContentSliderComponent["previous"];
   next: ContentSliderComponent["next"];
   elementCount: number;
-  currentContent?: string;
+  currentContent: string;
+  activeItemWidth: number;
+  inactiveItemWidth: number;
+  activeClass: string;
+  activeColumnClasses: string[];
+  inactiveColumnClasses: string[];
 }
 
 export class ContentSliderComponent extends Component {
@@ -17,8 +23,10 @@ export class ContentSliderComponent extends Component {
 
   protected autobind = true;
 
+  protected contentSliderEl: HTMLElement | null = null;
+
   static get observedAttributes() {
-    return ["hello"];
+    return [];
   }
 
   protected scope: Scope = {
@@ -28,12 +36,17 @@ export class ContentSliderComponent extends Component {
     previous: this.previous,
     next: this.next,
     elementCount: 0,
-    currentContent: undefined,
+    currentContent: "",
+    activeItemWidth: 0,
+    inactiveItemWidth: 0,
+    activeClass: "active",
+    activeColumnClasses: ["col-8", "col-md-4"],
+    inactiveColumnClasses: ["col-4", "col-md-2"],
   };
 
   constructor(element?: HTMLElement) {
     super(element);
-    console.debug("constructor", this);
+    this.debug("constructor", this);
   }
 
   protected connectedCallback() {
@@ -49,17 +62,44 @@ export class ContentSliderComponent extends Component {
 
   protected async beforeBind() {
     await super.beforeBind();
-    console.debug("beforeBind", this.scope);
+    this.debug("beforeBind", this.scope);
+  }
+
+  protected initItems() {
+    const items = this.el.querySelectorAll(".content-slider-item");
+    if (!items) {
+      throw new Error("No required items found!");
+    }
+    this.scope.elementCount = items.length;
+    this.scope.currentElement = 0;
+
+    for (let i = 0; i < this.scope.elementCount; i++) {
+      const item = items[i];
+      this.setInactiveClasses(item);
+    }
+
+    this.goTo(this.scope.currentElement);
   }
 
   protected async afterBind() {
     await super.afterBind();
-    console.debug("afterBind", this.scope);
-    const children = this.el.querySelector(".content-slider")?.children;
-    if (children) {
-      this.scope.elementCount = children.length;
+    this.debug("afterBind", this.scope);
+    this.contentSliderEl = this.el.querySelector<HTMLElement>(
+      ".content-slider"
+    );
+    if (!this.contentSliderEl) {
+      throw new Error("Missing element with selector .content-slider!");
     }
-    this.updateContent();
+
+    const contentInfoOffsetEl = this.el.querySelector<HTMLElement>(
+      ".content-slider-buttons"
+    );
+    if (contentInfoOffsetEl) {
+      contentInfoOffsetEl.classList.add;
+      this.addClasses(contentInfoOffsetEl, this.scope.activeColumnClasses);
+    }
+
+    this.initItems();
   }
 
   protected requiredAttributes() {
@@ -85,70 +125,149 @@ export class ContentSliderComponent extends Component {
     return super.disconnectedCallback();
   }
 
-  public next() {
-    if (!this.el && this.el !== null) {
-      return;
-    }
+  protected getItemElementByIndex(index: number) {
+    return this.el.querySelectorAll(".content-slider-item")?.item(index);
+  }
 
-    this.el
-      .querySelector(".content-slider-active")
-      ?.classList.remove("content-slider-active");
-    this.el
-      .querySelector(".content-slider")
-      ?.children[this.scope.currentElement + 1].classList.add(
-        "content-slider-active"
-      );
-    this.scope.currentElement++;
-    console.log("next", this.scope.currentElement);
-    this.updateContent();
+  protected addClasses(el: Element, classes: string[]) {
+    for (const curClass of classes) {
+      el.classList.add(curClass);
+    }
+  }
+  protected removeClasses(el: Element, classes: string[]) {
+    for (const curClass of classes) {
+      el.classList.remove(curClass);
+    }
+  }
+
+  protected setInactiveClasses(el: Element) {
+    this.removeClasses(el, [
+      ...this.scope.activeColumnClasses,
+      this.scope.activeClass,
+    ]);
+
+    this.addClasses(el, this.scope.inactiveColumnClasses);
+  }
+
+  protected setActiveClasses(el: Element) {
+    this.removeClasses(el, this.scope.inactiveColumnClasses);
+
+    this.addClasses(el, [
+      ...this.scope.activeColumnClasses,
+      this.scope.activeClass,
+    ]);
+  }
+
+  public next() {
+    const newActiveIndex = this.scope.currentElement + 1;
+    this.goTo(newActiveIndex);
   }
 
   public previous() {
+    const newActiveIndex = this.scope.currentElement - 1;
+    this.goTo(newActiveIndex);
+  }
+
+  public goTo(newActiveIndex: number) {
     if (!this.el && this.el !== null) {
+      console.error("Component is not ready!");
       return;
     }
 
-    this.el
-      .querySelector(".content-slider-active")
-      ?.classList.remove("content-slider-active");
-    this.el
-      .querySelector(".content-slider")
-      ?.children[this.scope.currentElement - 1].classList.add(
-        "content-slider-active"
-      );
-    this.scope.currentElement--;
+    this.getItemWidths();
+
+    const oldActiveItem = this.el.querySelector(".active");
+    const newActiveItem = this.getItemElementByIndex(newActiveIndex);
+    if (oldActiveItem) {
+      this.setInactiveClasses(oldActiveItem);
+    } else {
+      console.warn("No old active item found!");
+    }
+
+    newActiveItem.classList.add("active");
+
+    if (newActiveItem) {
+      this.setActiveClasses(newActiveItem);
+    } else {
+      console.warn("No new active item found!");
+    }
+
+    this.scope.currentElement = newActiveIndex;
     console.log("previous", this.scope.currentElement);
     this.updateContent();
+  }
+
+  protected getItemWidths() {
+    this.scope.activeItemWidth =
+      this.el
+        .querySelector(".content-slider-item.active")
+        ?.getBoundingClientRect().width || 0;
+
+    this.scope.inactiveItemWidth =
+      this.el
+        .querySelector(".content-slider-item:not(.active)")
+        ?.getBoundingClientRect().width || 0;
+
+    console.log("activeItemWidth", this.scope.activeItemWidth);
+    console.log("inactiveItemWidth", this.scope.inactiveItemWidth);
+  }
+
+  protected getTranslateXForIndex(positionIndex: number) {
+    this.scope.activeItemWidth =
+      this.el
+        .querySelector(".content-slider-item.active")
+        ?.getBoundingClientRect().width || 0;
+
+    this.scope.inactiveItemWidth =
+      this.el
+        .querySelector(".content-slider-item:not(.active)")
+        ?.getBoundingClientRect().width || 0;
+
+    if (positionIndex <= 0) {
+      return 0;
+    } else if (positionIndex === 1) {
+      return this.scope.activeItemWidth;
+    } else if (positionIndex > 1) {
+      return (
+        this.scope.activeItemWidth +
+        this.scope.inactiveItemWidth * (positionIndex - 1)
+      );
+    }
+    return 0;
   }
 
   public updateContent() {
     console.log(this.scope.currentElement);
 
-    (this.el.querySelector(".content-slider") as HTMLElement).style.transform =
-      "translateX(calc(calc(" +
-      this.scope.currentElement +
-      " * -20vw) - calc(" +
-      this.scope.currentElement +
-      " * 2vw)))";
+    if (!this.contentSliderEl) {
+      throw new Error("Missing element with selector .content-slider!");
+    }
 
-    this.scope.currentContent = this.el.querySelector(
-      ".content-sliderls"
-    )?.children[this.scope.currentElement].children[0].innerHTML;
+    // (this.el.querySelector(".content-slider") as HTMLElement).style.transform =
+    //   "translateX(calc(calc(" +
+    //   this.scope.currentElement +
+    //   " * -20%) - calc(" +
+    //   this.scope.currentElement +
+    //   " * 20%)))";
+
+    const x = this.getTranslateXForIndex(this.scope.currentElement);
+
+    this.contentSliderEl.style.transform = `translateX(-${x}px)`;
+
+    this.scope.currentContent = this.getItemElementByIndex(
+      this.scope.currentElement
+    ).children[0].innerHTML;
     //console.log("translateX(calc(calc(" + this.scope.currentElement + " * -20vw) - calc(" + this.scope.currentElement + " * 1vw))");
   }
 
   protected template() {
     // Only set the component template if there no childs already
     if (this.el.hasChildNodes()) {
-      console.debug("Do not use template, because element has child nodes");
+      this.debug("Do not use template, because element has child nodes");
       return null;
     } else {
-      console.debug("Use template", template);
+      this.debug("Use template", template);
       return template;
     }
   }
 }
-
-/*
-translateX(calc( calc(0 * -20vw) - calc( this.scope.currentElement * 2vw)));
-*/
