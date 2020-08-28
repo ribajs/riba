f6f0fe5edee89383b82b4052fbd270bd4939a38b
art-and-code-studio <pascal@artandcode.studio>
2020-08-28 15:20:18 +0200

    [Schematics] replace console.debug with this.debug and other small changes

--- infra/schematics/.eslintrc.js
+++ infra/schematics/.eslintrc.js
@@ -2,5 +2,5 @@
 module.exports = {
   root: true,
   extends: ["@ribajs"],
-  ignorePatterns: ["**/*.bundle.js", "src/lib/*/files/*"],
+  ignorePatterns: ["**/*.bundle.js", "src/lib/**/files/*", "**/__name__.*"],
 };
