25486f6db1aabcd029de973ed90b34a7a0c7391a
Daniel <dotonesimple@gmail.com>
2020-09-10 17:42:42 +0200

    Improved timer code

--- packages/moment/src/components/timer/timer.component.ts
+++ packages/moment/src/components/timer/timer.component.ts
@@ -2,19 +2,36 @@ import { Component } from "@ribajs/core";
 
 interface Scope {
   time: string;
+  countdownDays: boolean;
+  countdownHours: boolean;
+  countdownMinutes: boolean;
+  countdownSeconds: boolean;
+
   countdownRunning: boolean;
+
+  remainingDays: number;
   remainingHours: number;
   remainingMinutes: number;
   remainingSeconds: number;
 }
 
+///TODO: Add "timer" mode, which starts at a given point in time,
+// and counts the time passed since than, and a mixed
+// mode, which is basically a countdown and after the countdown reached 0,
+// it continues as a "timer".
 export class TimerComponent extends Component {
   public static tagName = "moment-timer";
 
   protected autobind = true;
 
   static get observedAttributes() {
-    return ["time"];
+    return [
+      "time",
+      "countdown-days",
+      "countdown-hours",
+      "countdown-minutes",
+      "countdown-seconds",
+    ];
   }
 
   protected requiredAttributes() {
@@ -22,14 +39,21 @@ export class TimerComponent extends Component {
   }
 
   protected scope: Scope = {
+    countdownDays: true,
+    countdownHours: true,
+    countdownMinutes: true,
+    countdownSeconds: true,
+
     countdownRunning: true,
+
+    remainingDays: 0,
     remainingHours: 0,
     remainingMinutes: 0,
     remainingSeconds: 0,
     time: "",
   };
 
-  private timeObject?: Date;
+  private updateIntervalId?: number;
 
   constructor(element?: HTMLElement) {
     super(element);
@@ -38,25 +62,7 @@ export class TimerComponent extends Component {
   protected connectedCallback() {
     super.connectedCallback();
     this.init(TimerComponent.observedAttributes);
-    console.log("connected", this.scope);
-    this.updateCountdown();
-    setInterval(() => {
-      this.updateCountdown();
-    }, 1000);
-  }
-
-  protected updateCountdown() {
-    let remainingSeconds =
-      (Date.parse(this.scope.time) - new Date().getTime()) / 1000;
-    console.log(remainingSeconds);
-
-    this.scope.remainingHours = Math.floor(remainingSeconds / (60 * 60));
-    remainingSeconds -= this.scope.remainingHours * 60 * 60;
-
-    this.scope.remainingMinutes = Math.floor(remainingSeconds / 60);
-    remainingSeconds -= this.scope.remainingMinutes * 60;
-
-    this.scope.remainingSeconds = Math.ceil(remainingSeconds);
+    this.startCountdownInterval();
   }
 
   protected async init(observedAttributes: string[]) {
@@ -71,9 +77,100 @@ export class TimerComponent extends Component {
     await super.afterBind();
   }
 
+  protected updateCountdown() {
+    //calculate seconds until countdown is over
+    let remainingSeconds = this.getRemainingSeconds();
+    if (!remainingSeconds || remainingSeconds < 1) {
+      this.cancelCountdownInterval();
+      this.resetCountdown();
+      return;
+    }
+
+    if (this.scope.countdownDays) {
+      if (this.isDaysLastUnit()) {
+        this.scope.remainingDays = Math.ceil(remainingSeconds / (24 * 60 * 60));
+        return;
+      } else {
+        this.scope.remainingDays = Math.floor(
+          remainingSeconds / (24 * 60 * 60)
+        );
+        remainingSeconds -= this.scope.remainingDays * 24 * 60 * 60;
+      }
+    }
+
+    if (this.scope.countdownHours) {
+      if (this.isHoursLastUnit()) {
+        this.scope.remainingHours = Math.ceil(remainingSeconds / (60 * 60));
+        return;
+      } else {
+        this.scope.remainingHours = Math.floor(remainingSeconds / (60 * 60));
+        remainingSeconds -= this.scope.remainingHours * 60 * 60;
+      }
+    }
+
+    if (this.scope.countdownMinutes) {
+      if (this.isMinutesLastUnit()) {
+        this.scope.remainingMinutes = Math.ceil(remainingSeconds / 60);
+        return;
+      } else {
+        this.scope.remainingMinutes = Math.floor(remainingSeconds / 60);
+        remainingSeconds -= this.scope.remainingMinutes * 60;
+      }
+    }
+
+    this.scope.remainingSeconds = Math.ceil(remainingSeconds);
+  }
+
+  protected resetCountdown() {
+    this.scope.remainingDays = 0;
+    this.scope.remainingHours = 0;
+    this.scope.remainingMinutes = 0;
+    this.scope.remainingSeconds = 0;
+  }
+
+  //returns remaining seconds until this.scope.time is reached.
+  protected getRemainingSeconds(): number {
+    return Math.ceil(
+      (Date.parse(this.scope.time) - new Date().getTime()) / 1000
+    );
+  }
+
+  protected isDaysLastUnit(): boolean {
+    return (
+      this.scope.countdownHours == false &&
+      this.scope.countdownMinutes == false &&
+      this.scope.countdownSeconds == false
+    );
+  }
+
+  protected isHoursLastUnit(): boolean {
+    return (
+      this.scope.countdownMinutes == false &&
+      this.scope.countdownSeconds == false
+    );
+  }
+  protected isMinutesLastUnit(): boolean {
+    return this.scope.countdownSeconds == false;
+  }
+
+  protected startCountdownInterval() {
+    //call function immediatly to
+    this.updateCountdown();
+    //create interval and save id to clear later to prevent resource leaking
+    this.updateIntervalId = setInterval(() => {
+      this.updateCountdown();
+    }, 1000);
+  }
+
+  protected cancelCountdownInterval() {
+    if (this.updateIntervalId) clearInterval(this.updateIntervalId);
+    this.updateIntervalId = undefined;
+  }
+
   // deconstructor
   protected disconnectedCallback() {
     super.disconnectedCallback();
+    this.cancelCountdownInterval();
   }
 
   protected template() {
