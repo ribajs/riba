51b05bb21a1cb68e891f416fc802d1e695bd775f
Moritz Raguschat <moritz@jumplink.me>
2020-09-13 16:28:39 +0700

    [Core:mapFormatter Test] register required adapter

--- packages/core/src/formatters/special/map.formatter.spec.ts
+++ packages/core/src/formatters/special/map.formatter.spec.ts
@@ -1,7 +1,8 @@
-import { Riba, textBinder } from '@ribajs/core';
+import { Riba, textBinder, dotAdapter } from '@ribajs/core';
 import { MapFormatter } from './map.formatter';
 
 const riba = new Riba();
+riba.module.adapter.regist(dotAdapter);
 riba.module.formatter.regist(MapFormatter);
 riba.module.binder.regist(textBinder);
 
