5f2ad318461951a62d7141ec9ddc9aeb3d155785
Moritz Raguschat <moritz@jumplink.me>
2020-09-29 05:37:15 +0700

    [BS4] PopoverBinder: expose popover methods through events

--- packages/bs4/src/binders/bs4-popover.binder.ts
+++ packages/bs4/src/binders/bs4-popover.binder.ts
@@ -21,11 +21,63 @@ export const popoverBinder: Binder<string> = {
     }
     options.placement = options.placement || "auto";
 
-    this.customData.popover = new PopoverService(el, {
+    const popover = new PopoverService(el, {
       ...PopoverService.Default,
       ...options,
     });
+
+    // destroy previous popover if it already exists
+    if (this.customData.popover) {
+      this.customData.popover.dispose();
+    }
+
+    this.customData.popover = popover;
+
+    /*
+     * Methods "show", "hide", etc. of the PopoverService can be called by dispatching an
+     * event `trigger-${methodName}` on the bound element.
+     * All these methods have no arguments.
+     */
+    const methodNames: (keyof typeof popover)[] = [
+      "show",
+      "hide",
+      "toggle",
+      "dispose",
+      "enable",
+      "disable",
+      "toggleEnabled",
+      "update", // render update
+    ];
+
+    // remove listeners of previous Popover if there already was one
+    if (this.customData.listeners) {
+      for (const [trigger, listener] of Object.entries(
+        this.customData.listeners as [string, EventListener]
+      )) {
+        this.el.removeEventListener(trigger, listener as EventListener);
+      }
+    }
+
+    this.customData.listeners = [];
+    for (const methodName of methodNames) {
+      const listener = (popover[methodName] as any).bind(popover);
+      this.el.addEventListener(`trigger-${methodName}`, listener);
+      this.customData.listeners.push(listener);
+    }
   },
 
-  // unbind() {},
+  unbind() {
+    // destroy Popover if it already exists
+    if (this.customData.popover) {
+      this.customData.popover.dispose();
+    }
+    // remove listeners if there are any
+    if (this.customData.listeners) {
+      for (const [trigger, listener] of Object.entries(
+        this.customData.listeners as [string, EventListener]
+      )) {
+        this.el.removeEventListener(trigger, listener as EventListener);
+      }
+    }
+  },
 };
