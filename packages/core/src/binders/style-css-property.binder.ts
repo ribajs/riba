3bd81ee1b54e143bd0723b26184c391db47af378
Moritz Raguschat <moritz@jumplink.me>
2020-10-01 09:46:14 +0700

    [Core] fix css property binder for css variables

--- packages/core/src/binders/style-css-property.binder.ts
+++ packages/core/src/binders/style-css-property.binder.ts
@@ -12,10 +12,13 @@ export const cssStarBinder: Binder<string> = {
   name: "style-*",
   routine(el: HTMLElement, value: string) {
     const propertyName = this.args[0];
+    // if (this.args.length >= 2) {
+    console.error(`THIS FUCKING ARGS:`, this.args, value);
+    //}
     if (value === null || value === undefined || value === "") {
       (el.style as any).removeProperty(propertyName);
     } else {
-      (el.style as any)[propertyName] = value;
+      (el.style as any).setProperty(propertyName, value);
     }
   },
 };
