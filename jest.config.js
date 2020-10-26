351037381788b42f093e2dc9a8c8e3c123425c9e
art-and-code-studio <pascal@artandcode.studio>
2020-10-26 12:19:08 +0100

    [Test] Revert downgrade jest

--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,38 @@
 /* eslint-disable no-undef */
 module.exports = {
-  testEnvironment: "jsdom",
-  moduleFileExtensions: ["ts", "tsx", "js", "json"],
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'ts',
+    'tsx',
+  ],
   resetModules: true,
   transform: {
     "^.+\\.tsx?$": require.resolve("ts-jest"),
   },
-  testRegex: "/src/.*\\.(test|spec).(ts|tsx)$",
+  testRegex: '/src/.*\\.(test|spec).(ts|tsx)$',
   transformIgnorePatterns: [
-    "node_modules/(?!(@ribajs)/)",
-    "\\.pnp\\.[^\\\/]+$"
+    'node_modules/(?!(@ribajs)/)',
+    '\\.pnp\\.[^\\/]+$',
   ],
   testPathIgnorePatterns: [
-    "/infra/schematics/dist/lib/",
-    "/infra/schematics/src/lib/",
-    "/node_modules/",
-    "/packages/*/dist/"
+    '/infra/schematics/dist/lib/',
+    '/infra/schematics/src/lib/',
+    '/node_modules/',
+    '/packages/*/dist/',
   ],
-  coverageReporters: ["json", "lcov"],
+  coverageReporters: [
+    'json',
+    'lcov',
+  ],
   setupFilesAfterEnv: [require.resolve("jest-extended")],
   globals: {
-    "ts-jest": {
+    'ts-jest': {
       babelConfig: './babel.config.js',
-      tsconfig: "./tsconfig.spec.json",
+      tsconfig: './tsconfig.spec.json',
     },
   },
-};
+  preset: 'ts-jest',
+  testMatch: null,
+}
