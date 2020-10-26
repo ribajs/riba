6ef4a6f3648e6cbdff33942eb89f03fa7c6b3e77
art-and-code-studio <pascal@artandcode.studio>
2020-10-26 14:33:12 +0100

    [Webpack] Autoload webpack config on serve script

--- infra/webpack-config/serve.js
+++ infra/webpack-config/serve.js
@@ -1,26 +1,40 @@
 /* eslint-disable no-case-declarations */
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
+const path = require("path");
 const webpack = require("webpack");
 const WDS = require("webpack-dev-server");
+const rootPath = process.cwd();
 
-const ribaWebpackConfig = require("./index");
+const start = () => {
+  let webpackConfig;
+  try {
+    webpackConfig = require(path.resolve(rootPath, "webpack.config.js"))({
+      production: false,
+      development: true,
+    });
+    console.debug("Use webpack config");
+  } catch (error) {
+    webpackConfig = require("./index")({ template: "local" })({
+      production: false,
+      development: true,
+    });
+    console.debug("Use default local config");
+  }
+  // console.debug(webpackConfig);
 
-module.exports = (config = {}) => {
-  const webpackConfig = ribaWebpackConfig(config)({
-    production: false,
-    development: true,
-  });
   WDS.addDevServerEntrypoints(webpackConfig, webpackConfig.devServer);
 
   const compiler = webpack(webpackConfig);
   const devServer = new WDS(compiler, webpackConfig.devServer);
 
   devServer.listen(
-    webpackConfig.devServer.port,
-    webpackConfig.devServer.host,
+    webpackConfig.devServer.port || 8080,
+    webpackConfig.devServer.host || "0.0.0.0",
     (err) => {
       // noop
     }
   );
 };
+
+module.exports = start();
