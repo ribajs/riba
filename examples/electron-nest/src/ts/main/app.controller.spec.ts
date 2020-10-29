9a95f905e7680c89384cbc843be0cbfc4365aef8
art-and-code-studio <pascal@artandcode.studio>
2020-10-29 15:42:10 +0100

    [Test] Fix electron-nest test

--- examples/electron-nest/src/ts/main/app.controller.spec.ts
+++ examples/electron-nest/src/ts/main/app.controller.spec.ts
@@ -2,21 +2,23 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-describe('AppController', () => {
-  let appController: AppController;
+describe('examples/electron-nest', () => {
+  describe('AppController', () => {
+    let appController: AppController;
 
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
-    }).compile();
+    beforeEach(async () => {
+      const app: TestingModule = await Test.createTestingModule({
+        controllers: [AppController],
+        providers: [AppService],
+      }).compile();
 
-    appController = app.get<AppController>(AppController);
-  });
+      appController = app.get<AppController>(AppController);
+    });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    describe('root', () => {
+      it('should have a versions method', () => {
+        expect(appController.versions).toBeFunction();
+      });
     });
   });
 });
