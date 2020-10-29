78983a8ca4d3d46a401d1a39d4c02622292ae599
art-and-code-studio <pascal@artandcode.studio>
2020-10-29 15:31:23 +0100

    [Webpack] Add support for define plugin

--- demos/vue/webpack.config.js
+++ demos/vue/webpack.config.js
@@ -4,39 +4,31 @@
 /**
  * For a example vue webpack configuration see https://github.com/vuejs/vue-loader/blob/master/example/webpack.config.js
  *
- * TODO make use of vue-style-loader
+ * TODO make use of vue-style-loader?
  */
 
 const { VueLoaderPlugin } = require("vue-loader");
 const ribaWebpackConfig = require("@ribajs/webpack-config");
-const webpackConfig = ribaWebpackConfig({ template: "local" });
-module.exports = (env) => {
-  const envConfig = webpackConfig(env);
-
-  const vueEsmPath = require.resolve("vue/dist/vue.esm-bundler.js");
-  console.log("vueEsmPath", vueEsmPath);
-
-  // resolve vue
-  envConfig.resolve = envConfig.resolve || {};
-  envConfig.resolve.alias = envConfig.resolve.alias || {};
-  envConfig.resolve.alias["vue"] = vueEsmPath;
-
-  // vue loader rule
-  envConfig.module = envConfig.module || {};
-  envConfig.module.rules = envConfig.module.rules || [];
-  envConfig.module.rules.unshift({
+const webpackConfig = ribaWebpackConfig({
+  template: "local",
+  define: {
+    __VUE_OPTIONS_API__: "true",
+    __VUE_PROD_DEVTOOLS__: "false"
+  },
+  rules: [{
     test: /\.vue$/,
     loader: "vue-loader",
-  });
-
-  // vue loader plugin
-  envConfig.plugins = envConfig.plugins || [];
-  envConfig.plugins.push(new VueLoaderPlugin());
-
-  // Resolve vue-loader
-  envConfig.resolveLoader = envConfig.resolveLoader || {};
-  envConfig.resolveLoader.alias = envConfig.resolveLoader.alias || {};
-  envConfig.resolveLoader.alias["vue-loader"] = require.resolve("vue-loader");
-
+  }],
+  resolve: {
+    alias: {
+      "vue": require.resolve("vue/dist/vue.esm-bundler.js"),
+    }
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+});
+module.exports = async (env) => {
+  const envConfig = await webpackConfig(env);
   return envConfig;
 };
--- infra/webpack-config/index.js
+++ infra/webpack-config/index.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-case-declarations */
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
+const webpack = require("webpack");
 const path = require("path");
 const pkgDir = require('pkg-dir');
 const rootPath = pkgDir.sync(process.cwd());
-// const webpack = require("webpack");
 
 var getStyleLoaderRule = (config = {}) => {
   var rule = {
@@ -99,9 +99,20 @@ module.exports = (config = {}) => {
     config.production = env.production;
     config.development = env.development;
 
-    var plugins = [];
+    config.resolve = config.resolve || {
+      symlinks: true,
+      alias: {},
+      plugins: [],
+    };
+    config.resolve.extensions = config.resolve.extensions || [".ts", ".tsx", ".js", ".json", ".scss", ".pug", ".html"];
+    config.resolve.alias 
+    config.resolve.symlinks = true;
 
-    var rules = [
+    config.plugins = config.plugins || [];
+
+    config.rules = config.rules || [];
+
+    config.rules.push(
       // typescript and javascript
       {
         test: /\.(tsx?)|\.(js)$/,
@@ -141,7 +152,7 @@ module.exports = (config = {}) => {
           },
         ],
       },
-    ];
+    );
 
     // config defaults
     config.detectDuplicates = config.detectDuplicates || false;
@@ -234,7 +245,7 @@ module.exports = (config = {}) => {
         };
 
         const HtmlWebpackPlugin = require("html-webpack-plugin");
-        plugins.push(
+        config.plugins.push(
           new HtmlWebpackPlugin({
             template: path.resolve(rootPath, "src/index.html"),
             filename: "index.html",
@@ -266,17 +277,17 @@ module.exports = (config = {}) => {
         config.CopyPlugin = config.CopyPlugin || require("copy-webpack-plugin");
         // Copy the files before the build starts for the case the files are required for the build itself
         copy(copyPluginConfigs.patterns);
-        plugins.push(new config.CopyPlugin(copyPluginConfigs));
+        config.plugins.push(new config.CopyPlugin(copyPluginConfigs));
       }
     }
 
     if (config.detectDuplicates === true) {
       const { DuplicatesPlugin } = require("inspectpack/plugin");
-      plugins.push(new DuplicatesPlugin());
+      config.plugins.push(new DuplicatesPlugin());
     }
 
     if (config.styles.extract === true) {
-      plugins.push(
+      config.plugins.push(
         new config.CssExtractPlugin({
           filename: "[name].css",
         })
@@ -285,54 +296,56 @@ module.exports = (config = {}) => {
 
     // if (config.development) {
     //   console.debug("Use HotModuleReplacementPlugin");
-    //   plugins.push(new webpack.HotModuleReplacementPlugin());
+    //   config.plugins.push(new webpack.HotModuleReplacementPlugin());
     // }
 
-    // console.debug('Used plugins: ', plugins);
+    // console.debug('Used plugins: ', config.plugins);
 
     if (config.styles.build === true) {
-      rules.push(getStyleLoaderRule(config));
+      config.rules.push(getStyleLoaderRule(config));
     }
 
-    const webpackConfig = {
-      optimization: {
-        minimize: config.scripts.minimize,
-        minimizer: config.scripts.minimize ? [terser] : [],
-        splitChunks: config.splitChunks || {
-          // TODO refactor see https://webpack.js.org/migrate/5/
-          automaticNameDelimiter: ".",
-          chunks: "all",
-          cacheGroups: {
-            commons: {
-              test: /[\\/]node_modules[\\/]/,
-              name: "vendors",
-              chunks: "all",
-            },
-            styles: {
-              name: "styles",
-              test: /\.css$/,
-              chunks: "all",
-              enforce: true,
-            },
+    // Define plugin
+    config.define = config.define || {};
+    config.define.ENV = JSON.stringify(env);
+    config.plugins.push(new webpack.DefinePlugin(config.define));
+
+
+    config.optimization = config.optimization || {
+      minimize: config.scripts.minimize,
+      minimizer: config.scripts.minimize ? [terser] : [],
+      splitChunks: config.splitChunks || {
+        // TODO refactor see https://webpack.js.org/migrate/5/
+        automaticNameDelimiter: ".",
+        chunks: "all",
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: "vendors",
+            chunks: "all",
+          },
+          styles: {
+            name: "styles",
+            test: /\.css$/,
+            chunks: "all",
+            enforce: true,
           },
         },
       },
-      // Change to your "entry-point".
+    };
+
+    const webpackConfig = {
+      optimization: config.optimization,
       entry: config.entry,
       devtool: env.production ? undefined : "inline-source-map",
       mode: env.production ? "production" : "development",
       output: config.output,
-      resolve: {
-        extensions: [".ts", ".tsx", ".js", ".json", ".scss", ".pug", ".html"],
-        symlinks: true,
-        alias: {},
-        plugins: [],
-      },
+      resolve: config.resolve,
       devServer: config.devServer,
       module: {
-        rules: rules,
+        rules: config.rules,
       },
-      plugins: plugins,
+      plugins: config.plugins,
     };
 
     if (config.target) {
