b0f4c5f30bf23f951c9e2015dbce64051e593601
trollkotze <moritz.raguschat@web.de>
2020-12-31 04:42:23 +0000

    [Events] fix package build

--- packages/events/dist/services/index.js
+++ packages/events/dist/services/index.js
@@ -1,6 +1,2 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EventDispatcher = void 0;
-var event_dispatcher_service_1 = require("./event-dispatcher.service");
-Object.defineProperty(exports, "EventDispatcher", { enumerable: true, get: function () { return event_dispatcher_service_1.EventDispatcher; } });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvc2VydmljZXMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQUEsdUVBQTZEO0FBQXBELDJIQUFBLGVBQWUsT0FBQSJ9
+export { EventDispatcher } from "./event-dispatcher.service";
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvc2VydmljZXMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsT0FBTyxFQUFFLGVBQWUsRUFBRSxNQUFNLDRCQUE0QixDQUFDIn0=
--- packages/events/dist/types/event-dispatcher.js
+++ packages/events/dist/types/event-dispatcher.js
@@ -1,3 +1,2 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+export {};
 //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZXZlbnQtZGlzcGF0Y2hlci5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy90eXBlcy9ldmVudC1kaXNwYXRjaGVyLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==
