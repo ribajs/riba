1e7208d23dff0e8e85a44fedf48a7e70546cf1ec
art-and-code-studio <pascal@artandcode.studio>
2021-02-25 13:42:35 +0100

    Upgrade dependencies

--- infra/eslint-config/index.js
+++ infra/eslint-config/index.js
@@ -3,10 +3,7 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
+    "prettier",
     "plugin:prettier/recommended",
   ],
   rules: {
--- packages/bs5/src/services/dom/index.ts
+++ packages/bs5/src/services/dom/index.ts
@@ -1,4 +1,3 @@
 export * from "./data";
 export * from "./event-handler";
 export * from "./manipulator";
-export * from "./selector-engine";
