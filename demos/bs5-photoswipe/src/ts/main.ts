5be71fba2af01327c9b3ed97a687d50adeb46423
Pascal Garber <pascal@artandcode.studio>
2021-05-07 13:25:23 +0200

    [Masonry] New masonry binder

--- demos/bs5-photoswipe/src/ts/main.ts
+++ demos/bs5-photoswipe/src/ts/main.ts
@@ -2,6 +2,7 @@ import { bs5Module } from "@ribajs/bs5";
 import { coreModule, Riba } from "@ribajs/core";
 import { extrasModule } from "@ribajs/extras";
 import { bs5PhotoswipeModule } from "@ribajs/bs5-photoswipe";
+import { masonryModule } from "@ribajs/masonry";
 import { PhotoswipeDemoModule } from "./photoswipe-demo.module";
 
 const riba = new Riba();
@@ -13,6 +14,7 @@ riba.module.regist(coreModule);
 riba.module.regist(bs5Module.init());
 riba.module.regist(extrasModule);
 riba.module.regist(bs5PhotoswipeModule);
+riba.module.regist(masonryModule);
 riba.module.regist(PhotoswipeDemoModule);
 
 riba.bind(document.body, model);
---
+++ packages/masonry/src/binders/masonry.binder.ts
@@ -0,0 +1,82 @@
+import { Binder, LifecycleService } from "@ribajs/core";
+import Masonry from "masonry-layout";
+import type { Options } from "masonry-layout";
+import { throttle } from "@ribajs/utils";
+
+/**
+ * masonry
+ * This binder can be used similar to the original data-attribute of Masonry,
+ * see the original documentation for the options
+ * @see https://masonry.desandro.com/options.html
+ *
+ * @example
+ * <div class="grid" rv-masonry='{ "columnWidth": 200, "itemSelector": ".grid-item" }'>
+ */
+export const masonryBinder: Binder<Options> = {
+  name: "masonry",
+
+  bind(el) {
+    const layout = () => {
+      const masonry = this.customData.masonry as Masonry | null;
+      console.debug("layout", masonry);
+      if (masonry?.layout) {
+        masonry.layout();
+      }
+    };
+
+    this.customData = {
+      masonry: null,
+      layout: throttle(layout.bind(this)),
+      images: null,
+      resizeObserver: null,
+    };
+
+    this.customData.resizeObserver = new ResizeObserver(this.customData.layout);
+
+    // All components bound
+    const lifecycle = LifecycleService.getInstance();
+    lifecycle.events.once(
+      "ComponentLifecycle:allBound",
+      this.customData.layout,
+      this
+    );
+  },
+
+  unbind() {
+    let masonry = this.customData.masonry as Masonry | null;
+    if (masonry?.destroy) {
+      masonry.destroy();
+      masonry = null;
+    }
+    this.customData.images.forEach((img: HTMLImageElement) => {
+      // Image size changed
+      this.customData.resizeObserver.unobserve(img);
+    });
+    delete this.customData;
+  },
+
+  routine(el: HTMLElement, options: Options = {}) {
+    if (this.customData?.masonry?.destroy) {
+      this.customData.masonry.destroy();
+    }
+    this.customData.masonry = new Masonry(el, options);
+
+    this.customData.images = el.querySelectorAll<HTMLImageElement>("img");
+
+    // All components bound
+    const lifecycle = LifecycleService.getInstance();
+    lifecycle.events.once(
+      "ComponentLifecycle:allBound",
+      this.customData.layout,
+      this
+    );
+
+    // Detect image changes
+    this.customData.images.forEach((img: HTMLImageElement) => {
+      // Image loaded
+      img.onload?.(this.customData.layout);
+      // Image size changed
+      this.customData.resizeObserver.observe(img);
+    });
+  },
+};
