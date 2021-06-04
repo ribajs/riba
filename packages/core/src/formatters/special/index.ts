8a31f38bd18499ae8b8465099ed49d87380e6d4f
Pascal Garber <pascal@artandcode.studio>
2021-06-04 22:17:30 +0000

    [Core] add ternary formatter

--- packages/core/src/formatters/special/index.ts
+++ packages/core/src/formatters/special/index.ts
@@ -6,3 +6,4 @@ export { callFormatter } from "./call.formatter";
 export { callNameFormatter } from "./call-name.formatter";
 export { mapFormatter } from "./map.formatter";
 export { preferFormatter } from "./prefer.formatter";
+export { ternaryFormatter } from "./ternary.formatter";
