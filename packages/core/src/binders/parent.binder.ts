925258ef8e2e6a39369c33c2e37bb09d3b3fa069
trollkotze <moritz.raguschat@web.de>
2021-06-15 12:29:21 +0200

    [Core] parent binder can only bind after CustomElement is upgraded

--- packages/core/src/binders/parent.binder.ts
+++ packages/core/src/binders/parent.binder.ts
@@ -11,13 +11,45 @@ export const parentBinder: Binder<any, BasicComponent> = {
     /**/
   },
   bind(el) {
-    if (el.setBinderAttribute) {
-      el.setBinderAttribute("$parent", this.view.models);
-    } else {
+    const customConstructor = customElements.get(el.localName);
+    if (!customConstructor) {
       console.warn(
         "[parentBinder] You can only use this binder on Riba components",
-        el
+        el.localName,
       );
+    } else if (el.constructor === customConstructor) {
+      if (el.setBinderAttribute) {
+        el.setBinderAttribute("$parent", this.view.models);
+      } else {
+        console.warn(
+          "[parentBinder] You can only use this binder on Riba components",
+          el.localName,
+          customElements.get(el.localName)
+        );
+      }
+    } else {
+      console.debug(
+        `[parentBinder] CustomElement ${el.localName} has been defined, but not yet upgraded. Waiting for upgrade.`,
+        el,
+        el.constructor,
+        customConstructor
+      );
+      window.customElements.whenDefined(el.localName).then((customElementConstructor) => {
+        if (el.setBinderAttribute) {
+          console.debug(`[parentBinder] Upgraded component ${el.localName}, binding $parent`,
+            el,
+            customElementConstructor,
+            this.view.models
+          );
+          el.setBinderAttribute("$parent", this.view.models);
+        } else {
+          console.warn(
+            "[parentBinder] You can only use this binder on Riba components",
+            el.localName,
+            customElements.get(el.localName)
+          );
+        }
+      })
     }
   },
 };
