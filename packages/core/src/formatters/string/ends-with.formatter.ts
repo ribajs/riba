706abae1d43c6b0042f8b5c86dbc0aed6e0e8773
Pascal Garber <pascal@artandcode.studio>
2021-06-23 19:20:04 +0000

    [Core] New ends-with string formatter

---
+++ packages/core/src/formatters/string/ends-with.formatter.ts
@@ -0,0 +1,20 @@
+import { Formatter } from "../../types";
+
+/**
+ * Returns true if the sequence of elements of searchString converted to a String is the same as the corresponding elements of this object (converted to a String) starting at endPosition – length(this).
+ * Otherwise returns false.
+ *
+ * @example
+ * <p rv-if="'abcdefg' | ends-with 'efg'">Hello World!</p>
+ * -> <p>Hello World!</p>
+ */
+export const endsWithFormatter: Formatter = {
+  name: "ends-with",
+  read(str: string, checkStart: string) {
+    if (typeof str !== "string") {
+      console.warn("[endsWithFormatter] Value must be of type string");
+      return str;
+    }
+    return str.endsWith(checkStart);
+  },
+};
