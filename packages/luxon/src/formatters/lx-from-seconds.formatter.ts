a87f157e672152b97e5fbb668125ec22d360eeb3
trollkotze <moritz.raguschat@web.de>
2021-06-23 21:08:53 +0200

    [Luxon] Fix some exports

--- packages/luxon/src/formatters/lx-from-seconds.formatter.ts
+++ packages/luxon/src/formatters/lx-from-seconds.formatter.ts
@@ -5,7 +5,7 @@ import { DateTime, DateTimeOptions } from "luxon";
  * Gets a luxon date from number of seconds since the epoch (meaning since January 1, 1970, 00:00:00 UTC).
  * @see https://moment.github.io/luxon/docs/class/src/datetime.js~DateTime.html#static-method-fromSeconds
  */
-export const LuxonFromObjectFormatter: Formatter = {
+export const LuxonFromSecondsFormatter: Formatter = {
   name: "lx-from-seconds",
   /**
    * @param seconds number
