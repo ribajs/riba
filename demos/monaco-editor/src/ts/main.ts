8ac38c28109b1429aa2ac2945dd0766d79156493
Pascal Garber <pascal@artandcode.studio>
2021-11-30 18:28:01 +0100

    [Demo] Fix more demo builds

--- demos/monaco-editor/src/ts/main.ts
+++ demos/monaco-editor/src/ts/main.ts
@@ -1,6 +1,4 @@
-import { bs5Module } from "@ribajs/bs5";
 import { coreModule, Riba } from "@ribajs/core";
-import { extrasModule } from "@ribajs/extras";
 import { monacoEditorModule } from "@ribajs/monaco-editor";
 import { MonacoDemoModule } from "./monaco-demo.module";
 
@@ -9,8 +7,6 @@ const model = {};
 
 // Register modules
 riba.module.regist(coreModule.init());
-riba.module.regist(bs5Module.init());
-riba.module.regist(extrasModule.init());
 riba.module.regist(monacoEditorModule.init());
 riba.module.regist(MonacoDemoModule.init());
 
