940d3e4a0e048cdaf9f85d850f793cdd4fdf9225
Pascal Garber <pascal@artandcode.studio>
2022-05-05 12:04:25 +0200

    Bump version 2.0.0-alpha.4

--- packages/shopify/src/shopify.module.ts
+++ packages/shopify/src/shopify.module.ts
@@ -1,18 +1,17 @@
 import { RibaModule } from "@ribajs/core";
 import * as binders from "./binders/index.js";
-import * as components from "./components/index.js";
 import * as formatters from "./formatters/index.js";
-import * as services from "./services/index.js";
+import { ModuleService } from "./services/index.js";
 import { ShopifyModuleOptions } from "./types/index.js";
 
 export const shopifyModule: RibaModule<ShopifyModuleOptions> = {
   binders,
   formatters,
-  services,
-  components,
+  services: {},
+  components: {},
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   init(options = {}) {
-    services.ModuleService.setSingleton(options);
+    ModuleService.setSingleton(options);
     return this;
   },
 };
