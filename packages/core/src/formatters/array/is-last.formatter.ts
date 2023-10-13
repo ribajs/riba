13389fdf2aaf6625e89642e6927ed354f85e3ecc
Pascal Garber <pascal@artandcode.studio>
2023-10-13 10:07:28 +0200

    Upgrade dependencies

--- packages/core/src/formatters/array/is-last.formatter.ts
+++ packages/core/src/formatters/array/is-last.formatter.ts
@@ -1,7 +1,7 @@
 /**
  * Returns true if value index is the last index of the array. Returns false if it is not the last index.
  * ```
- *  <div rv-each-image="product.images" rv-hide="product.images | last %image%"></div>
+ *  <div rv-each-image="product.images" rv-hide="product.images | isLast %image%"></div>
  * ```
  * @see https://help.shopify.com/themes/liquid/objects/for-loops#forloop-last
  */
